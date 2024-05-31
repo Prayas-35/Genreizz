8372d4451da87a76b85e268e9c4c09b604e500f8
Prayas <prayaspal04@gmail.com>
2024-05-31 20:31:29 +0530

    scrolling animation

--- static/main.js
+++ static/main.js
@@ -41,3 +41,21 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#scrollToTopBtn').fadeIn();
+        } else {
+            $('#scrollToTopBtn').fadeOut();
+        }
+    });
+
+    $('#scrollToTopBtn').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'slow');
+        return false;
+    });
+})
