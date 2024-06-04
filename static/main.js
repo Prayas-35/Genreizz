cd7eb3c38376146f3ee380be9cddba33386bd5c0
Prayas <prayaspal04@gmail.com>
2024-06-05 02:06:29 +0530

    restructed and added tooltip

--- static/main.js
+++ static/main.js
@@ -80,3 +80,6 @@ $(document).ready(function () {
         }
     });
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
